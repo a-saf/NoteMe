06c600bba4f8ab2935a6e54c1575f8870f6bd8d8
Danial Asghar <danial.aw@gmail.com>
2021-09-28 17:51:50 -0400

    lab finished

--- app/src/main/java/com/sofe4640/noteme/NewNoteActivity.java
+++ app/src/main/java/com/sofe4640/noteme/NewNoteActivity.java
@@ -1,8 +1,15 @@
 package com.sofe4640.noteme;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class NewNoteActivity extends AppCompatActivity {
 
@@ -10,5 +17,22 @@ public class NewNoteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_note);
+
+        //Create AppBar and set Back button
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+        ab.setDisplayShowTitleEnabled(false);
+
+        //Save button navigate back to home screen
+        FloatingActionButton saveButton = findViewById(R.id.new_note_btn);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
