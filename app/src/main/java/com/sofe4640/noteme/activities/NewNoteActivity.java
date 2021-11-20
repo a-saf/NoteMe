01e4e7d9882dbb3517c46c86de6c0f6f1ba1c8b9
Yusuf-shaik <yusuf.shaik@uoit.net>
2021-11-20 17:59:51 -0500

    final lab

--- app/src/main/java/com/sofe4640/noteme/activities/NewNoteActivity.java
+++ app/src/main/java/com/sofe4640/noteme/activities/NewNoteActivity.java
@@ -1,6 +1,8 @@
 package com.sofe4640.noteme.activities;
 
 import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
@@ -10,7 +12,9 @@ import android.graphics.Color;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -27,14 +31,20 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.sofe4640.noteme.db.DBHandler;
 import com.sofe4640.noteme.R;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Objects;
 
 import petrov.kristiyan.colorpicker.ColorPicker;
@@ -46,9 +56,12 @@ public class NewNoteActivity extends AppCompatActivity {
     int colorPicked;
     ImageView imageNote;
     private String selectedImagePath;
+    String currentPhotoPath;
+    File photoFile;
 
     private static final int REQUEST_CODE_STORAGE_PERMISSION = 1;
     private static final int REQUEST_CODE_SELECT_IMAGE = 2;
+    private static final int CAMERA_REQUEST = 1888;
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
@@ -99,9 +112,40 @@ public class NewNoteActivity extends AppCompatActivity {
             case R.id.add_image:
                 imagePermissions();
                 break;
+            case R.id.open_camera:
+                addImage();
+                break;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void addImage(){
+
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                System.out.println("erorr when creating file");
+                // Error occurred while creating the File
+
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.sofe4640.noteme.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                //takePictureIntent.putExtra("uri", photoURI);
+
+                startActivityForResult(takePictureIntent, CAMERA_REQUEST);
+            }
+        }
+
     }
 
     public void openColorPicker(){
@@ -164,9 +208,11 @@ public class NewNoteActivity extends AppCompatActivity {
         }
     }
 
+
     private void selectImage(){
         Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         startActivityForResult(intent, REQUEST_CODE_SELECT_IMAGE);
+
 
     }
 
@@ -203,6 +249,44 @@ public class NewNoteActivity extends AppCompatActivity {
                 }
             }
         }
+        System.out.println(requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK);
+
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK)
+        {
+            InputStream inputStream = null;
+            try {
+                inputStream = getContentResolver().openInputStream(Uri.fromFile(photoFile));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+            imageNote.setImageBitmap(bitmap);
+            imageNote.setVisibility(View.VISIBLE);
+
+            selectedImagePath = getPathFromUri(Uri.fromFile(photoFile));
+            System.out.println(selectedImagePath);
+//            Bitmap photo = (Bitmap) data.getExtras().get("data");
+//            imageNote.setImageBitmap(photo);
+
+
+        }
+    }
+
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
     }
 
     private String getPathFromUri(Uri contentUri){
